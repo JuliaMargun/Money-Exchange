a5f62033a3e3764cb8a703c60ebe9317d4c4318e
JuliaMargun <yuliya.marhun@gmail.com>
2018-02-23 21:26:53 +0300

    A method makeExchange that will determine the minimum number of coins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	needdedMoney = {}
+    
+    if (currency <= 0) {
+    	return needdedMoney;
+
+    } else if (currency > 10000) {
+    	needdedMoney.error = "You are rich, my friend! We don't have so much coins for exchange";
+    	return needdedMoney;
+    	
+    } else {
+    	if (currency >= 50) {
+    		needdedMoney.H = currency / 50 | 0;
+    		currency -= needdedMoney.H * 50;
+    	}
+
+    	if (currency >= 25) {
+    		needdedMoney.Q = currency / 25 | 0;
+    		currency -= needdedMoney.Q * 25;
+    	}
+
+    	if (currency >= 10) {
+    		needdedMoney.D = currency / 10 | 0;
+    		currency -= needdedMoney.D * 10;
+    	}
+
+    	if (currency >= 5) {
+    		needdedMoney.N = currency / 5 | 0;
+    		currency -= needdedMoney.N * 5;
+    	}
+
+    	if (currency > 0) {
+    		needdedMoney.P = currency;
+    	}
+
+    	return needdedMoney;
+    }
 }
